b146663a864dd4221ab8a2e3960bf61b6eea173a
Moksh parashar <parasharm239@gmail.com>
2024-01-06 19:57:13 -0800

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Moksh's App</title>
+      </head>
       <body className={inter.className}>
         <Header />
         {children}
